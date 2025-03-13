d16fcabcb455a1112574bd20392df5a10cf67482
bhavan-16 <bhavanp2004@gmail.com>
2025-03-13 22:53:27 +0530

    Create Bid.java

---
+++ Bid.java
@@ -0,0 +1,18 @@
+public class Bid {
+    private final User bidder;
+    private final double amount;
+
+    public Bid(User bidder, double amount) {
+        this.bidder = bidder;
+        this.amount = amount;
+    }
+
+    public User getBidder() {
+        return bidder;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
+
